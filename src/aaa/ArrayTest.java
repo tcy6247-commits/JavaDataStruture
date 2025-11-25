c87655fa8b55b30ce539f9b4bdf0fe6ba185d608
tam131 <tcy6247@gmail.com>
2025-11-25 16:55:33 +0800

    2

--- src/aaa/ArrayTest.java
+++ src/aaa/ArrayTest.java
@@ -1,9 +1,9 @@
-package src;
+package aaa;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] arr={8,2,7,3,1};
-        for (int i = 0; i < arr.length; i++) {
+        int[] arr = {5,1,7,4,8};
+        for(int i=0;i< arr.length;i++){
             System.out.println(arr[i]);
         }
     }

a89b289b09ec6fa17433d4a716a472aeb94769e7
tam131 <tcy6247@gmail.com>
2025-11-25 16:41:29 +0800

    1

---
+++ src/src/ArrayTest.java
@@ -0,0 +1,10 @@
+package src;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr={8,2,7,3,1};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
